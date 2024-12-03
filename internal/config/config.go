82fe604a05fe08b571a4ccd596e077dfacec21f0
letsmakecakes <adwaithrajeev@gmail.com>
2024-12-03 20:27:24 +0530

    Initial commit.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,20 @@
 package config
+
+import "sync"
+
+// Source represents different configuration sources
+type Source string
+
+const (
+	SourceEnvFile     Source = "env_file"
+	SourceEnvironment Source = "environment"
+	SourceConfigFile  Source = "config_file"
+)
+
+// Config represents the application configuration
+type Config struct {
+	// Mutex to ensure thread-safe access
+	mu sync.RWMutex
+
+	//
+}
