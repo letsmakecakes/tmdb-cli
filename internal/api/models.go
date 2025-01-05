3eda7248f67cbae953a1d2a847feb8de22c5941d
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-01-05 21:19:06 +0530

    Added data model for API response.

---
+++ internal/api/models.go
@@ -0,0 +1,31 @@
+package api
+
+type MovieResponse struct {
+	Dates        Dates   `json:"dates,omitempty"`
+	Page         int     `json:"page"`
+	Results      []Movie `json:"results"`
+	TotalPages   int     `json:"total_pages"`
+	TotalResults int     `json:"total_results"`
+}
+
+type Dates struct {
+	Maximum string `json:"maximum"`
+	Minimum string `json:"minimum"`
+}
+
+type Movie struct {
+	Adult            bool    `json:"adult"`
+	BackdropPath     string  `json:"backdrop_path"`
+	GenreIds         []int   `json:"genre_ids"`
+	ID               int     `json:"id"`
+	OriginalLanguage string  `json:"original_language"`
+	OriginalTitle    string  `json:"original_title"`
+	Overview         string  `json:"overview"`
+	Popularity       float64 `json:"popularity"`
+	PosterPath       string  `json:"poster_path"`
+	ReleaseDate      string  `json:"release_date"`
+	Title            string  `json:"title"`
+	Video            bool    `json:"video"`
+	VoteAverage      float64 `json:"vote_average"`
+	VoteCount        int     `json:"vote_count"`
+}
